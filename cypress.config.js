4b26d3458bd0dc8dbffd6fbabb6ee74c0471c766
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-07-07 19:11:54 -0300

    feat: created report

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br',
-    // reporter: 'mochawesome',
-    // reporterOptions: {
-    //   reportDir: 'mochawesome-report',
-    //   overwrite: false,
-    //   html: false,
-    //   json: true
-    // }
+      reporter: 'mochawesome',
+      reporterOptions: {
+      reportDir: 'mochawesome-report',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   }
 });
